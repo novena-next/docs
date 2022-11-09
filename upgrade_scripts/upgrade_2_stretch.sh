11900cfa45bfcdae757b65b63c87717a7586ad77
anon8675309 <anon8675309@localhost>
2022-11-09 01:38:17 -0600

    fix: Added missing PGP keys for apt repos #30

--- upgrade_scripts/upgrade_2_stretch.sh
+++ upgrade_scripts/upgrade_2_stretch.sh
@@ -9,6 +9,13 @@ sed -i 's/jessie/stretch/g' /etc/apt/sources.list
 # if it is not removed first
 rm /etc/apt/sources.list.d/kosagi.list || true
 rm /etc/apt/trusted.gpg.d/kosagi.gpg || true
+# We will also need some additional keys to get updates
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key AA8E81B4331F7F50
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key 112695A0E562B32A
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key 04EE7237B7D453EC
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key 648ACFD622F3D138
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key 0E98404D386FA1D9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key EF0F382A1A7B6500
 apt update
 apt-get -y -oAPT::Force-LoopBreak=1 dist-upgrade
 
