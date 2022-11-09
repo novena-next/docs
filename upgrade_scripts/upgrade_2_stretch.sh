8285d5b867d95c5186b5c79ace37526885be42f0
anon8675309 <anon8675309@localhost>
2022-11-09 01:35:21 -0600

    fix: failure to 'apt upgrade' See issue #29

--- upgrade_scripts/upgrade_2_stretch.sh
+++ upgrade_scripts/upgrade_2_stretch.sh
@@ -5,6 +5,10 @@ if [[ "$(id -u)" != "0" ]]; then
    exit 1
 fi
 sed -i 's/jessie/stretch/g' /etc/apt/sources.list
+# The kosagi repo doesn't resolve anymore and will cause an error in apt update
+# if it is not removed first
+rm /etc/apt/sources.list.d/kosagi.list || true
+rm /etc/apt/trusted.gpg.d/kosagi.gpg || true
 apt update
 apt-get -y -oAPT::Force-LoopBreak=1 dist-upgrade
 
